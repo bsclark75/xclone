214f3bab1ac3ff9cd2263f263fb47448aa9e3359
Brian Clark <bsclark75@gmail.com>
2025-08-26 19:24:43 -0400

    Finish static Pages

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,17 @@ main = Blueprint("main", __name__)
 
 @main.route("/")
 def index():
-    return render_template("base.html", title="Home")
+    return render_template("home.html", title="Home")
+
+@main.route("/home")
+def home():
+    return render_template("home.html", title="Home")
+
+@main.route("/help")
+def help():
+    return render_template("help.html", title="Help")
+
+@main.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,4 +10,19 @@ def client():
 def test_index(client):
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Welcome to Flask Scaffold" in response.data
+    assert b"<title>Home</title>" in response.data
+
+def test_home(client):
+    response = client.get("/home")
+    assert response.status_code == 200
+    assert b"This is the home page" in response.data
+
+def test_help(client):
+    response = client.get("/help")
+    assert response.status_code == 200
+    assert b"This is a holding page" in response.data
+
+def test_about(client):
+    response = client.get("/about")
+    assert response.status_code == 200
+    assert b"This is an about page" in response.data
